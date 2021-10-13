34d4b1c51c018ae3657bc4658a25719f5b050547
Gavin Birkhoff <gavinbirkhoff@gmail.com>
2021-10-13 20:07:06 +0800

    strategy

--- README.md
+++ README.md
@@ -1,10 +1,17 @@
 # design-patterns-ts
 《大话设计模式》案例忠于原书。使用typescript实现，相比c++，java来说js更能快速描述一些抽象思想。设计模式只是一种思想，用伪代码实现其实更好！
+
+> 类型安全性只做基本考虑，让精力侧重在设计模式之上
+
 # Use
 ```
 npm install
 npm run test
 ```
 # design-patterns
-- [工厂模式](./src/factory.ts)
+### 工厂模式
+[代码无措就是优？](./src/factory.ts)
+
+### 策略模式
+[商场促销](./src/strategy.ts)
 
---
+++ src/strategy.ts
@@ -0,0 +1,95 @@
+import { switchCase } from '@babel/types'
+
+abstract class Strategy {
+  // 算法方法
+  public abstract algorithmInterface(): void
+}
+
+class ConcreteStrategyA extends Strategy {
+  public algorithmInterface() {}
+}
+class ConcreteStrategyB extends Strategy {
+  public algorithmInterface() {}
+}
+class ConcreteStrategyC extends Strategy {
+  public algorithmInterface() {}
+}
+
+// 上下文
+class Context {
+  strategy: Strategy
+  // 初始化传入具体的策略对象
+  constructor(strategy: Strategy) {
+    this.strategy = strategy
+  }
+  // 上下文接口
+  public contextInterface() {
+    // 根据具体的策略对象调用其算法对象
+    this.strategy.algorithmInterface()
+  }
+}
+
+let context = new Context(new ConcreteStrategyA())
+context.contextInterface()
+// 以上是模型
+
+abstract class CashSuper {
+  public abstract acceptCash(money: number): number
+}
+
+class CashNormal extends CashSuper {
+  public acceptCash(money: number): number {
+    return money
+  }
+}
+
+class CashRebate extends CashSuper {
+  private moneyRebate = 1
+  constructor(moneyRebate: string) {
+    super()
+    this.moneyRebate = parseInt(moneyRebate)
+  }
+  public acceptCash(money: number): number {
+    return money * this.moneyRebate
+  }
+}
+
+class CashReturn extends CashSuper {
+  private moneyCondition = 0.0
+  private moneyReturn = 0.0
+  constructor(moneyCondition: string, moneyReturn: string) {
+    super()
+    this.moneyCondition = parseInt(moneyCondition)
+    this.moneyReturn = parseInt(moneyReturn)
+  }
+  public acceptCash(money: number): number {
+    let result = money
+    if (money >= this.moneyCondition) {
+      result = money - Math.floor(money / this.moneyCondition) * this.moneyReturn
+    }
+    return result
+  }
+}
+
+class CashContext {
+  private cs: CashSuper | undefined
+  constructor(type: string) {
+    switch (type) {
+      case '正常收费':
+        this.cs = new CashNormal()
+        break
+      case '满300返100':
+        this.cs = new CashReturn('300', '100')
+        break
+      case '打8折':
+        this.cs = new CashRebate('0.8')
+        break
+      default:
+        new Error('必须先选好策略')
+        break
+    }
+  }
+  public getResult(money: number): number {
+    return this.cs!.acceptCash(money)
+  }
+}
